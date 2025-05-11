c09d294b9b21969983f123881deeb56b74e917f4
Kevin <ivfrvek@gmail.com>
2025-05-11 20:11:29 +0200

    Test avec if

--- etl/extract.py
+++ etl/extract.py
@@ -3,8 +3,10 @@ import os
 
 #Fonction pour importé le fichier csv et le traités
 def extract_data(filepath):
+
     #Variable dataframe pour lire le csv
     df = pd.read_csv(filepath)
+
     #Lecture des 5 premiers résultats
     print("print des 5 premiere valeur", df[:5])
     print("Fais la meme choses que df[:5]", df.head())
@@ -12,6 +14,15 @@ def extract_data(filepath):
     #Lecture du tableau
     print("Forme du tableau", df.shape)
 
-    return df
+    check_columns = list(df.columns)
+    print(check_columns)
 
+    expect_columns = ['timestamp', 'sensor_id', 'temperature']
+    if check_columns != expect_columns:
+        print("Erreur des colonnes")
+
+    print("Colonne lues :", list(df.columns))
+
+    return df
+print("Colonne lues :", list(df.columns))
 extract_data("data/simulated/sensor_data.csv")
